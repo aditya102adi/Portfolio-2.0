ae2ad7e8d1e3306c8a21821967b9bebcdc0fb800
Aditya Kumar Sharma <75057567+aditya102adi@users.noreply.github.com>
2023-04-07 12:08:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -36,20 +36,17 @@ window.addEventListener("scroll", scrollHeader)
 const sections = document.querySelectorAll("section[id]");
 
 window.addEventListener("scroll", navHigh);
-function navHigh()
-{
-  let scrollY=window.pageYOffset;
+function navHigh() {
+  let scrollY = window.pageYOffset;
   sections.forEach(current => {
     const sectionHeight = current.offsetHeight();
     const sectionTop = current.offsetTop - 58,
-    sectionId = current.getAttribute("id");
+      sectionId = current.getAttribute("id");
 
-    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight)
-    {
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
       document.querySelector('.nav-menu a[href*=' + sectionId + ']').add("active-link")
     }
-    else
-    {
+    else {
       document.querySelector('.nav-menu a[href*=' + sectionId + ']').remove("active-link")
     }
   })
@@ -61,7 +58,7 @@ const theme = document.querySelector("#theme-button");
 const themeModal = document.querySelector(".customize-theme");
 const fontSizes = document.querySelectorAll('.choose-size span');
 const colorPallet = document.querySelectorAll('.choose-color span');
-var root=document.querySelector(":root");
+var root = document.querySelector(":root");
 const Bg1 = document.querySelector(".bg-1");
 const Bg2 = document.querySelector(".bg-2");
 const Bg3 = document.querySelector(".bg-3");
@@ -83,31 +80,27 @@ themeModal.addEventListener("click", closeThemeModel);
 /*===== FONTS =====*/
 
 //remove active class from spans or font-size selectors
-const removeSizeSelector = () =>{
+const removeSizeSelector = () => {
   fontSizes.forEach(size => {
     size.classList.remove("active");
   })
 }
 fontSizes.forEach(size => {
   size.addEventListener('click', () => {
-    
+
     removeSizeSelector();
     let fontSize;
     size.classList.toggle('active');
-    if (size.classList.contains('font-size-1')) 
-    {
+    if (size.classList.contains('font-size-1')) {
       fontSize = '12px';
-    } 
-    else if (size.classList.contains('font-size-2')) 
-    {
+    }
+    else if (size.classList.contains('font-size-2')) {
       fontSize = '14px';
-    } 
-    else if (size.classList.contains('font-size-3')) 
-    {
+    }
+    else if (size.classList.contains('font-size-3')) {
       fontSize = '16px';
     }
-    else if (size.classList.contains('font-size-4')) 
-    {
+    else if (size.classList.contains('font-size-4')) {
       fontSize = '18px';
     }
 
@@ -118,34 +111,29 @@ fontSizes.forEach(size => {
 /*===== PRIMARY COLORS =====*/
 
 //remove active class
-const changeActiveColorClass =() => {
-  colorPallet.forEach(colorPicker =>{
+const changeActiveColorClass = () => {
+  colorPallet.forEach(colorPicker => {
     colorPicker.classList.remove('active');
   })
 }
-colorPallet.forEach(color =>{
-  color.addEventListener('click', ()=>{
+colorPallet.forEach(color => {
+  color.addEventListener('click', () => {
     let primaryHue;
     changeActiveColorClass();
 
-    if(color.classList.contains('color-1'))
-    {
+    if (color.classList.contains('color-1')) {
       primaryHue = 252;
     }
-    else if(color.classList.contains('color-2'))
-    {
+    else if (color.classList.contains('color-2')) {
       primaryHue = 52;
     }
-    else if(color.classList.contains('color-3'))
-    {
+    else if (color.classList.contains('color-3')) {
       primaryHue = 352;
     }
-    else if(color.classList.contains('color-4'))
-    {
+    else if (color.classList.contains('color-4')) {
       primaryHue = 152;
     }
-    else if(color.classList.contains('color-5'))
-    {
+    else if (color.classList.contains('color-5')) {
       primaryHue = 202;
     }
     color.classList.add("active");
@@ -166,16 +154,16 @@ const changeBG = () => {
 }
 
 //CHANGE BACKGROUND COLOR 
-Bg1.addEventListener('click',()=>{
+Bg1.addEventListener('click', () => {
 
   Bg1.classList.add('active');
 
   Bg2.classList.remove('active');
   Bg3.classList.remove('active');
 
-  window.location.reload(); 
+  window.location.reload();
 })
-Bg2.addEventListener('click', ()=>{
+Bg2.addEventListener('click', () => {
   darkCB = '95%';
   whiteCB = '20%';
   lightCB = '15%';
@@ -187,7 +175,7 @@ Bg2.addEventListener('click', ()=>{
   Bg3.classList.remove('active');
   changeBG();
 })
-Bg3.addEventListener('click', ()=>{
+Bg3.addEventListener('click', () => {
   darkCB = '95%';
   whiteCB = '10%';
   lightCB = '0%';
@@ -199,3 +187,14 @@ Bg3.addEventListener('click', ()=>{
   Bg1.classList.remove('active');
   changeBG();
 })
+//GALLERY
+var fullImgBox = document.getElementById("fullImgBox");
+var fullImg = document.getElementById("fullImg");
+
+function openFullImg(pic) {
+  fullImgBox.style.display = "flex";
+  fullImg.src = pic;
+}
+function closeFullImg() {
+  fullImgBox.style.display = "none";
+}
